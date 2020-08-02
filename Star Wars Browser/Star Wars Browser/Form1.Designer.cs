263f323f32af53915d97803dd5b4f626974827a7
Zachary Lloyd <zaclloyd@uat.edu>
2020-08-02 15:32:13 -0400

    Project done

--- Star Wars Browser/Star Wars Browser/Form1.Designer.cs
+++ Star Wars Browser/Star Wars Browser/Form1.Designer.cs
@@ -28,25 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.Browser = new System.Windows.Forms.WebBrowser();
             this.LoadWebsite = new System.Windows.Forms.Button();
             this.URL = new System.Windows.Forms.TextBox();
             this.URL_Label = new System.Windows.Forms.Label();
+            this.HomeButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Browser
             // 
-            this.Browser.Location = new System.Drawing.Point(0, 80);
+            this.Browser.Location = new System.Drawing.Point(24, 80);
             this.Browser.MinimumSize = new System.Drawing.Size(20, 20);
             this.Browser.Name = "Browser";
-            this.Browser.Size = new System.Drawing.Size(799, 371);
+            this.Browser.Size = new System.Drawing.Size(1043, 525);
             this.Browser.TabIndex = 0;
             // 
             // LoadWebsite
             // 
             this.LoadWebsite.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LoadWebsite.Location = new System.Drawing.Point(12, 24);
+            this.LoadWebsite.Location = new System.Drawing.Point(24, 27);
             this.LoadWebsite.Name = "LoadWebsite";
             this.LoadWebsite.Size = new System.Drawing.Size(88, 41);
             this.LoadWebsite.TabIndex = 1;
@@ -59,7 +59,7 @@
             this.URL.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.URL.Location = new System.Drawing.Point(152, 36);
             this.URL.Name = "URL";
-            this.URL.Size = new System.Drawing.Size(375, 22);
+            this.URL.Size = new System.Drawing.Size(590, 22);
             this.URL.TabIndex = 2;
             // 
             // URL_Label
@@ -72,12 +72,23 @@
             this.URL_Label.TabIndex = 3;
             this.URL_Label.Text = "Search the Jedi Library";
             // 
+            // HomeButton
+            // 
+            this.HomeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HomeButton.Location = new System.Drawing.Point(979, 28);
+            this.HomeButton.Name = "HomeButton";
+            this.HomeButton.Size = new System.Drawing.Size(88, 40);
+            this.HomeButton.TabIndex = 4;
+            this.HomeButton.Text = "Home";
+            this.HomeButton.UseVisualStyleBackColor = true;
+            this.HomeButton.Click += new System.EventHandler(this.HomeButton_Click);
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1079, 642);
+            this.Controls.Add(this.HomeButton);
             this.Controls.Add(this.URL_Label);
             this.Controls.Add(this.URL);
             this.Controls.Add(this.LoadWebsite);
@@ -95,6 +106,7 @@
         private System.Windows.Forms.Button LoadWebsite;
         private System.Windows.Forms.TextBox URL;
         private System.Windows.Forms.Label URL_Label;
+        private System.Windows.Forms.Button HomeButton;
     }
 }
 
--- Star Wars Browser/Star Wars Browser/Form1.cs
+++ Star Wars Browser/Star Wars Browser/Form1.cs
@@ -12,14 +12,34 @@ namespace Star_Wars_Browser
 {
     public partial class FormMain : Form
     {
+        
+        string m_homePage = "https://starwars.fandom.com/wiki/Timeline_of_galactic_history";
         public FormMain()
         {
             InitializeComponent();
+            //Set the background image
+            this.BackgroundImage = Properties.Resources.JediLibrary;
+            //Automatically take the user to home page when Form is Initialized
+            Browser.Navigate(m_homePage);
+            //Used to disable any JavaScript or HTML errors a webpage so the user does not see them
+            Browser.ScriptErrorsSuppressed = true;
         }
 
         private void LoadWebsite_Click(object sender, EventArgs e)
         {
+            //Have the browser window navigate to the text in the url text box
             Browser.Navigate(URL.Text);
+            //Used to disable any JavaScript or HTML errors a webpage so the user does not see them
+            Browser.ScriptErrorsSuppressed = true;
+        }
+
+        private void HomeButton_Click(object sender, EventArgs e)
+        {
+            //Set the URL text box text equal to the home page string
+            URL.Text = m_homePage;
+            //Have the browser window navigate to the text in the url text box
+            Browser.Navigate(URL.Text);
+            //Used to disable any JavaScript or HTML errors a webpage so the user does not see them
             Browser.ScriptErrorsSuppressed = true;
         }
     }
